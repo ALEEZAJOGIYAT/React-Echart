61ff4f800d8a4b5b64705af0d4376f6d0e4181ba
ALEEZAJOGIYAT <alizajogyat09@gmail.com>
2023-12-27 00:04:34 +0500

    updated

--- src/components/sideBar/sideBar.tsx
+++ src/components/sideBar/sideBar.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Button, Layout, Menu } from 'antd';
-import { CloseOutlined, RightOutlined } from "@ant-design/icons";
-import { useMediaQuery } from "react-responsive";
-
+import { Button, Layout } from 'antd';
 import './style.css'
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+  } from '@ant-design/icons';
+  
 const {Sider } = Layout;
 
 
@@ -12,78 +14,60 @@ export const SideBar = () => {
     const [showDetails, setShowDetails] = useState(false);
     const [collapsed, setCollapsed] = useState(false);
 
-    const isMobile = useMediaQuery({ query: "(max-width: 700px)" });
-
+    const toggleCollapsed = () => {
+      setCollapsed(!collapsed);
+    } 
     const handleShowDetails = () => {
         setShowDetails(true)
     }
 
     return(
-        
-              {!isMobile ? (
-                    <Sider
-                    collapsed={collapsed}
-                    onCollapse={(value) => {
-                        console.log(value);
-                        setCollapsed(value);
-                    }}
-                    width={243}
-                    style={{
-                        overflow: "auto",
-                        height: "100vh",
-                        position: "fixed",
-                        left: 0,
-                        top: 0,
-                        bottom: 0,
-                        backgroundColor: "#4A0D37",
-                    }}
-                    >
-                        <div>
-                            <CloseOutlined onClick={() => setCollapsed(true)} />
+        <div>
 
-                            {collapsed && (
-                                <div onClick={() => setCollapsed(false)} className="collapsed_icon">
-                                <RightOutlined />
-                                </div>
-                            )}
-                        </div>
+            <div className="main-container">
+                <Button
+                    // type="primary"
+                    onClick={toggleCollapsed}
+                    style={{ marginTop: 16 }}
+                >
+                    <MenuUnfoldOutlined />
+                </Button>
+                {
+                    collapsed && 
 
-                    </Sider>
-              ) : 
-              <>
-        <div className="main-container">            
-            <Sider className="sidebar" theme="light" style={{height: '100%'}}>
-                <div className="main-heading">
-                    <h2>Summary</h2>
-                    <h3 style={{fontSize: '12px'}}>April 2018- Mar 2019</h3>
-                </div>
-                
-                <div className="sidebar-content">
-                    {showDetails && 
-                    
-                    <div className="options-list"> 
-                        <h4 className="options-list-content" style={{color:'blue', marginTop:'8px'}}>Over All - 24%</h4>
-                        <hr />
-                        <h4 className="options-list-content">Resignation Count</h4>
-                        <hr />
-                        <h4 className="options-list-content">Average Heads Count</h4>
-                        <hr />
-                        <h4 className="options-list-content" style={{color:'red'}}>High Performer - 160%</h4>
-                        <hr />
-                        <h4 className="options-list-content">Resignation Count</h4>
-                        <hr />
-                        <h4 className="options-list-content">Average Head Count</h4>
-                        <hr />
-                        <h4 className="options-list-content" style={{color:'green'}}>Performance</h4>
+                <Sider className="sidebar" theme="light" style={{height: '100%'}}>
+                    <div className="main-heading">
+                        <h2>Summary</h2>
+                        <h3 style={{fontSize: '12px'}}>April 2018- Mar 2019</h3>
                     </div>
-                    }
-                    <br/>
-                        <Button style={{width:'100%'}} onClick={handleShowDetails}>View Details</Button>
-                        <h4>Legend</h4>
-                </div>
-            </Sider>           
- 
+                    
+                    <div className="sidebar-content">
+                        {showDetails && 
+                        
+                        <div className="options-list"> 
+                            <h4 className="options-list-content" style={{color:'blue', marginTop:'8px'}}>Over All - 14.0%</h4>
+                            <hr />
+                            <h4 className="options-list-content">Resignation Count 639</h4>
+                            <hr />
+                            <h4 className="options-list-content">Average Heads Count 4.58K</h4>
+                            <hr />
+                            <h4 className="options-list-content" style={{color:'red'}}>High Performer - 14.1%</h4>
+                            <hr />
+                            <h4 className="options-list-content">Resignation Count - 152</h4>
+                            <hr />
+                            <h4 className="options-list-content">Average Head Count 108k</h4>
+                            <hr />
+                            <h4 className="options-list-content" style={{color:'green'}}>Performance -0.16p</h4>
+                        </div>
+                        }
+                        <br/>
+                            <Button style={{width:'100%'}} onClick={handleShowDetails}>View Details</Button>
+                            <h4>Legend</h4>
+                    </div>
+                </Sider>            
+                }
+
+            </div>
         </div>
-            </>
     )
 }
